5e30f133e9e3aa11ffa11db2ef1581c2b65c2827
mwolff <mwolff@build.wlg.afx.ovh>
2024-09-01 11:17:01 +0200

    Compatiblity with debian buster

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ set -e
 SRC=python-inotify
 
 git clone -b "debian/0.2.10-1" https://salsa.debian.org/python-team/packages/python-inotify.git "$SRC"
-cd "$SRC"
 
+quilt push -a
+
+cd "$SRC"
 sudo mk-build-deps --install --tool "apt-get --yes --no-install-recommends"
 exec dpkg-buildpackage -b -us -uc -tc
