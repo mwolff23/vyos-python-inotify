dc79b3b3ee22b106e3f668108d7874860f1d25c1
mwolff <mwolff@build.wlg.afx.ovh>
2024-09-01 13:50:47 +0200

    Use stdeb

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 set -e
 
-SRC=python-inotify
+wget -q 'https://files.pythonhosted.org/packages/35/cb/6d564f8a3f25d9516298dce151670d01e43a4b3b769c1c15f40453179cd5/inotify-0.2.10.tar.gz'
+tar xf inotify-0.2.10.tar.gz
 
-git clone -b "debian/0.2.10-1" https://salsa.debian.org/python-team/packages/python-inotify.git "$SRC"
-
-quilt push -a
-
+SRC="inotify-0.2.10"
 cd "$SRC"
-sudo mk-build-deps --install --tool "apt-get --yes --no-install-recommends"
-exec dpkg-buildpackage -b -us -uc -tc
+
+python3 setup.py --command-packages=stdeb.command bdist_deb
+mv -v -t ".." deb_dist/*.deb
